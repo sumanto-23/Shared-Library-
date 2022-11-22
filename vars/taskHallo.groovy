15ed915212b14e2fbadae74ab77e8319baa1eb2f
sumanto-23 <64468496+sumanto-23@users.noreply.github.com>
2022-11-22 20:31:59 +0530

    Update taskHallo.groovy

--- vars/taskHallo.groovy
+++ vars/taskHallo.groovy
@@ -1,10 +1,16 @@
 #!/usr/bin/env groovy
 def call(String name = 'test') {
   echo "Hello, ${name}."
-  1 * script.sh(
-                    """
-                    aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/o8e3g5w6
-                    
-                    """
-    )
 }
+ def "Test login to AWS ECR Docker Registry using AWS CLI 2"() {
+        when:
+            dockerEcr.loginToAWSECRDockerRegistry(2)
+        then:
+        1 * script.sh(
+                    """
+                    aws ecr get-login-password --region eu-west-2 \
+                    | docker login -u AWS https://066203203749.dkr.ecr.eu-west-2.amazonaws.com --password-stdin
+                    """
+        )
+    }
+
