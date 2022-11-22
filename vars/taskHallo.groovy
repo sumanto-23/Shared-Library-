ce1243f940fdc5f9d79a1d01d8e49bc649966af0
sumanto-23 <64468496+sumanto-23@users.noreply.github.com>
2022-11-22 20:24:45 +0530

    Update taskHallo.groovy

--- vars/taskHallo.groovy
+++ vars/taskHallo.groovy
@@ -1,6 +1,11 @@
 #!/usr/bin/env groovy
 def call(String name = 'test') {
   echo "Hello, ${name}."
+  1 * script.sh(
+                    """
+                    aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/o8e3g5w6
+                    
+                    """
 }
 def login() {
         1 * script.sh(
